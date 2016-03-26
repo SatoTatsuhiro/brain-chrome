0812b1513b872432b0e05973a8f9545a7419f7a4
tatsuleft <tatsuleft@satoujuyou-no-MacBook-Pro.local>
2016-03-26 14:00:28 +0900

    Initial commit

---
+++ Axiz Brain Chrome/Axiz Brain Chrome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Axiz Brain Chrome
+//
+//  Created by tatsuleft on 2016/03/26.
+//  Copyright (c) 2016年 tatsuleft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Axiz Brain Chrome/Axiz Brain ChromeTests/Axiz_Brain_ChromeTests.m
@@ -0,0 +1,40 @@
+//
+//  Axiz_Brain_ChromeTests.m
+//  Axiz Brain ChromeTests
+//
+//  Created by tatsuleft on 2016/03/26.
+//  Copyright (c) 2016年 tatsuleft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Axiz_Brain_ChromeTests : XCTestCase
+
+@end
+
+@implementation Axiz_Brain_ChromeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
